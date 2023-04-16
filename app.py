253e93893054f7562ada61c7ccd6a79b2f3ea899
Suwandi-amin-sangaji <suwandiaminsangaji@gmail.com>
2023-04-16 18:13:01 +0800

    aaaaaa

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ from collections import Counter
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+nltk.download('stopwords')
 
 # Halaman "Home"
 
